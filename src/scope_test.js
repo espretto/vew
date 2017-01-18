db0607a391902d62983c80a2fe48880b22c2ca95
Heiko Hübscher <heiko.huebscher@gmail.com>
2017-01-18 22:42:29 +0100

    [tests] replaces unmaintained expect.js w/ chai

--- src/scope_test.js
+++ src/scope_test.js
@@ -1,5 +1,5 @@
 
-import expect from 'expect.js'
+import { expect } from 'chai'
 import sinon from 'sinon'
 
 import Scope from './scope'
@@ -17,30 +17,30 @@ describe('Scope', function() {
   describe('#resolve', () => {
     it('should return merged value', () => {
       scope.merge({test: 'works for me'})
-      expect(scope.resolve('test')).to.be('works for me')
+      expect(scope.resolve('test')).to.equal('works for me')
     })
     it('should return merged nested value', () => {
       scope.merge({nested: {test: 'works for me'}})
-      expect(scope.resolve('nested.test')).to.be('works for me')
+      expect(scope.resolve('nested.test')).to.equal('works for me')
     })
     it('should return merged array', () => {
       scope.merge(['works for me', 'second'])
-      expect(scope.resolve('[0]')).to.be('works for me')
-      expect(scope.resolve('[1]')).to.be('second')
+      expect(scope.resolve('[0]')).to.equal('works for me')
+      expect(scope.resolve('[1]')).to.equal('second')
     })
     it('should return merged nested array', () => {
       scope.merge({nested: ['works for me', 'second']})
-      expect(scope.resolve('nested[0]')).to.be('works for me')
-      expect(scope.resolve('nested[1]')).to.be('second')
+      expect(scope.resolve('nested[0]')).to.equal('works for me')
+      expect(scope.resolve('nested[1]')).to.equal('second')
     })
     it('should return merged nested value in array', () => {
       scope.merge([{first: 'works for me'}, {second: 'second'}])
-      expect(scope.resolve('[0].first')).to.be('works for me')
-      expect(scope.resolve('[1].second')).to.be('second')
+      expect(scope.resolve('[0].first')).to.equal('works for me')
+      expect(scope.resolve('[1].second')).to.equal('second')
     })
     it('should return merged nested turtles', () => {
       scope.merge([{first: [,,{x: {y: {z: [,'works for me']}}}]}])
-      expect(scope.resolve('[0].first[2].x.y.z[1]')).to.be('works for me')
+      expect(scope.resolve('[0].first[2].x.y.z[1]')).to.equal('works for me')
     })
   })
 
@@ -53,22 +53,20 @@ describe('Scope', function() {
       scope.subscribe('test', callback)
       scope.merge({test: 'works for me'})
       scope.update()
-      expect(callback.called).to.be(true)
+      expect(callback.called).to.be.true
     })
     it('should not execute unsubscribed callback', () => {
       scope.subscribe('test', callback)
       scope.unsubscribe('test', callback)
       scope.merge({test: 'works for me'})
       scope.update()
-      expect(callback.called).to.be(false)
+      expect(callback.called).to.equal.false
     })
   })
 
   describe('#replace', () => {
     it('is not yet implemented', () => {
-      expect(scope.replace.bind(scope)).to.throwException((error) => {
-        expect(error.message).to.be('not yet implemented')
-      })
+      expect(scope.replace.bind(scope)).to.throw(/not yet implemented/)
     })
   })
 })
