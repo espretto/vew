aa31be116f0a6bbf0d96d9f57d65975515cab02b
espretto <espretto@gmail.com>
2017-01-20 14:44:28 +0100

    added working example

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function Vew (proto) {
 	}
 
 	Comp.bootstrap()
-	
+
   return Comp
 }
 
@@ -21,19 +21,18 @@ Vew.registry = registry
 /*
 	example:
 		
+		var Greet = Vew({
+			tag: 'greet',
+			replace: false,
+			template: 'Hello there!'
+		})
+
 		var App = Vew({
 			replace: false,
-			template: '<p><greet>${name}</greet></p>'
+			template: '<p><greet></greet></p>'
 		})
 
 		var app = App.create().mount(document.body)
-		
-		app.set({ name: 'Alice' })
-		app.scope.update()
-
-		app.set({ name: 'Bob' })
-		app.scope.update()
-
  */
 
 export default Vew
