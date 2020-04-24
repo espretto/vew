fd722547c30ba477df4696f6c171a6ab418f71ee
espretto <espretto@posteo.net>
2020-04-24 18:06:08 +0200

    [ops] configure {full,runtime} * {source,minified} builds

--- rollup.config.js
+++ rollup.config.js
@@ -9,56 +9,73 @@ import babel from 'rollup-plugin-babel'
 const { dependencies } = require('./package.json')
 const external = Object.keys(dependencies || {})
 
-export default {
-  input: 'src/component.js',
-  external,
-  plugins: [
-    resolve(),
-    babel({
-      exclude: 'node_modules/**'
-    })
-  ],
-  output: [
-    {
-      file: 'dist/runtime.js',
-      format: 'umd',
-      name: 'Vew',
-    },
-    {
-      file: 'dist/runtime.min.js',
-      format: 'umd',
-      name: 'Vew',
-      plugins: [
-        terser({
-          compress: {
+function createConfig ({ input, output }) {
+
+  console.assert(output.endsWith('.js'))
+  const minified = output.slice(0, -3) + '.min.js'
+
+  return {
+    input,
+    external,
+    plugins: [
+      resolve(),
+      babel({
+        exclude: 'node_modules/**'
+      })
+    ],
+    output: [
+      {
+        file: output,
+        format: 'umd',
+        name: 'Vew',
+      },
+      {
+        file: minified,
+        format: 'umd',
+        name: 'Vew',
+        plugins: [
+          terser({
+            compress: {
+              ecma: 5,
+              inline: 3,
+              hoist_funs: true,
+              hoist_props: true,
+              sequences: false,
+              toplevel: true,
+              pure_funcs: ['isNative'],
+              // make unofficial babel-plugin-transform-class work
+              // top_retain: ['extend', 'create'],
+              passes: 2,
+              dead_code: true
+            },
+            output: {
+              beautify: false,
+              ascii_only: true,
+              indent_level: 2,
+              max_line_len: 8196,
+              keep_quoted_props: true,
+            },
             ecma: 5,
-            inline: 3,
-            hoist_funs: true,
-            hoist_props: true,
-            sequences: false,
             toplevel: true,
-            pure_funcs: ['isNative'],
+            mangle: true,
             // make unofficial babel-plugin-transform-class work
-            // top_retain: ['extend', 'create'],
-            passes: 2,
-            dead_code: true
-          },
-          output: {
-            beautify: false,
-            ascii_only: true,
-            indent_level: 2,
-            max_line_len: 8196,
-            keep_quoted_props: true,
-          },
-          ecma: 5,
-          toplevel: true,
-          mangle: true,
-          // make unofficial babel-plugin-transform-class work
-          // mangle: { reserved: ['extend', 'create'] }
-          ie8: true,
-          safari10: true,
-        })
-      ]
-    }
-  ]
+            // mangle: { reserved: ['extend', 'create'] }
+            ie8: true,
+            safari10: true,
+          })
+        ]
+      }
+    ]
+  }
 }
+
+export default [
+  createConfig({
+    input: 'src/component.js',
+    output: 'dist/vew.runtime.js'
+  }),
+  createConfig({
+    input: 'src/index.js',
+    output: 'dist/vew.js'
+  })
+]
