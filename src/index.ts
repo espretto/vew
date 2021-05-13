a64cabecae03fb330782714e5be2896b797d0aff
espretto <espretto@posteo.net>
2021-05-13 16:23:39 +0200

    [lint] make it lint

--- src/index.ts
+++ src/index.ts
@@ -17,21 +17,24 @@ export function Component (options: ComponentOptions) {
 
   if (el) {
     const target = typeof el === 'string' ? document.querySelector(el) : el
+    console.assert(target, `could not find mount node ${el} for component ${options.tag}`)
+    // @ts-expect-error: target sure does exist
     return componentFactory(null).mount(target)
   }
 }
 
-Component.extend = function ({ tag, data, template }: ComponentOptions) {
+Component.extend = function ({ tag, data, template }: Omit<ComponentOptions, "el">) {
   let tmpl
 
   if (isString(template)) {
     const frag = parse(template)
 
     console.assert(
-      frag.firstChild.nodeType === 1 && frag.firstChild === frag.lastChild,
+      frag.firstChild?.nodeType === 1 && frag.firstChild === frag.lastChild,
       'component template must have a single root element node'
     )
 
+    // @ts-expect-error: we just asserted that the first child is an element
     tmpl = new Template(frag.removeChild(frag.firstChild))
   }
   else {
